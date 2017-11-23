d6384bba102ce73c6c4bd914fa6a6ac56a5d29f2
Tiziano <33886574+tittiamo68@users.noreply.github.com>
2017-11-23 02:20:31 +0100

    Add files via upload

---
+++ Lamp.ino
@@ -0,0 +1,33 @@
+
+void Lamp(){
+  LampOn1= EEPROM.read(0);
+  LampOff1 = EEPROM.read(1);
+  LampOn2 = EEPROM.read(2);
+  LampOff2 = EEPROM.read(3);
+  LampOn3 = EEPROM.read(4);
+  LampOff3 = EEPROM.read(5);
+
+     DateTime now = RTC.now();
+     if ( now.hour() >= LampOn1 && now.hour() <= LampOff1 )
+        {
+         digitalWrite(Lamp1,HIGH);// Lamp 1 On
+          }
+    else{
+         digitalWrite(Lamp1,LOW); // lamp 1 Off
+        }
+     if ( now.hour() >= LampOn2 && now.hour() <= LampOff2 )
+        {
+         digitalWrite(Lamp2,HIGH); // Lamp 2 On
+         }
+    else{
+         digitalWrite(Lamp2,LOW); // Lamp 2 Off
+        }
+     if ( now.hour() >= LampOn3 && now.hour() <= LampOff3 )
+        {
+         digitalWrite(Lamp3,HIGH); // Lamp 3 On
+         }
+    else{
+         digitalWrite(Lamp3,LOW); // Lamo 3 Off
+        }            
+} 
+
---
+++ Temperatura.ino
@@ -0,0 +1,28 @@
+void Temp18B20(){
+  sensors.requestTemperatures();
+  lcd.setCursor(0, 3);
+  lcd.print("Temp. H2o: ");
+  lcd.print (sensors.getTempCByIndex(0));
+  int TempH2o=sensors.getTempCByIndex(0);
+  
+  if(TempH2o < 25){
+  digitalWrite(Riscaldatore,HIGH); // Riscaldatore On
+  digitalWrite(Alarm,LOW); // Alarm Off
+  }
+  else if(TempH2o > 30){
+  digitalWrite(Riscaldatore,LOW); // Riscaldatore Off
+  digitalWrite(Alarm,HIGH); // Alarm On
+  }
+  else{
+    // optimal temperature
+    digitalWrite(Riscaldatore,LOW); // Riscaldatore Off
+    digitalWrite(Alarm,LOW); // Alarm Off
+  }
+}
+void TempDHT(){
+     int16_t t = dht.readTemperature(TEMPTYPE); // read temperature
+     lcd.setCursor(11,3);
+     lcd.print("Air:"); 
+     lcd.print(t);// Print temperature
+}
+
